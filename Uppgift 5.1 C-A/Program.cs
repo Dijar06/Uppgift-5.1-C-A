49e8eab4962d02611036322a1d2d90a21f5a667a
Dijar06 <dize06001@utb.vaxjo.se>
2023-11-28 14:57:42 +0100

    Add project files.

---
+++ Uppgift 5.1 C-A/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] v = new string[3] {"En mening", "En annan mening", "En tredje mening"};
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.WriteLine(v[i]);
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
